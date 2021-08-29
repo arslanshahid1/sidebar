937f131b991b58622ba983e22af036c8833da752
Arslan Shahid <arslanshahid1@gmail.com>
2021-08-29 12:34:23 +0500

    Add files via upload

---
+++ sidebar.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").classList.remove('close');
+    document.getElementById("mySidenav").classList.add('open');
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").classList.remove('open');
+    document.getElementById("mySidenav").classList.add('close');
+}
